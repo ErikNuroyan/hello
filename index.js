7b2ff87e878cf3b6375c9ca8a54dae53c5658d1a
Erik Nouroyan <Erik Nouroyan>
2016-09-07 14:57:02 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const a="Hello I'm Javascript!You found me!";
+console.log(a);
+const b=18;
+const c="My coder's age is ";
+console.log(c+b);
+const d="If you double my coder's age you will get ";
+console.log(d+2*b);
+const e="If you divide the age by 6 you'll get ";
+console.log(e + 2*b/6);
+
+
+
